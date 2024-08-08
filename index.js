e35f405e35cb618aa2a8e1752cb0916407de39d6
full-stack-hacks <redTapeRedTape@protonmail.com>
2024-08-08 15:24:05 -0500

    Added new stuff to the index and schema layers

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
 
 // server setup
-const server = new ApolloServer({});
+const server = new ApolloServer({
+  //typedefs
+  // typedefs are definition are the types I want to expose on my graph
+  // author, name, age, books, ect...
+  // type defs make up the schema
+  typeDefs,
+  //resolvers object - contains resolver functions. Tkane in a request and return
+  // data to the client (think schema is a map - resolvers actual resolve shit)
+});
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
